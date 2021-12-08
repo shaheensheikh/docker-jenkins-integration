8340adf353dbde054b145b29747ecedc0517cdd5
Shaheen Mohammad Ismail <shaheen.mohammad.ismail@ibm.com>
2021-12-08 11:35:57 +0530

    added space

--- src/main/java/com/example/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/example/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -9,6 +9,7 @@ public class DockerJenkinsIntegrationApplication {
     public static void main(String[] args) {
         SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
         System.out.println("welcome to docker-jenkins-integration sample");
+
     }
 
 }
